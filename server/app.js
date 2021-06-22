fdd78013ecd2933207cb937e9ed00a7fcddf1c79
akwong372 <akwong372@gmail.com>
2021-06-22 15:14:23 -0700

    installed graphql, start define schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 const PORT = 3000;
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+app.get('/', (req, res) => res.send('test'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
