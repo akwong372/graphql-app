4f27c2105888f3b5e3d81a679cef78d9a1fbe201
akwong372 <akwong372@gmail.com>
2021-06-23 16:13:19 -0700

    added mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3jusw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
 
 const app = express();
 const PORT = 3000;
