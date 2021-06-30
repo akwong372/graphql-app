632a49c599f0c15451beaa10edad20eb0b85d446
akwong372 <akwong372@gmail.com>
2021-06-29 22:05:59 -0700

    add BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import BookList from './components/BookList';
+
 function App() {
   return (
     <div className="App">
       <h1>some kind of book list</h1>
+      <BookList/>
     </div>
   );
 }
